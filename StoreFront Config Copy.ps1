4ed0336aa1078f6ccccec9d9bfe074b5814a17d0
scifidale <49734424+scifidale@users.noreply.github.com>
2020-05-18 11:35:21 +0100

    Create StoreFront Config Copy.ps1

---
+++ StoreFront Config Copy.ps1
@@ -0,0 +1,33 @@
+# Script to copy StoreFront configs from on server group to another
+# Script to copy StoreFront configs from on server group to another
+# IIS Site ID's must be the same for the Source and Destinations 
+# Powershell execution policy must be unrestricted
+# No warranties are given by running this script
+
+
+$Storename = "EEC"
+$RemoteSFServer = "CTXSFTest"
+$BackupFolder = "C:\Test"
+$RemoteDir = "Test"
+$HostBaseURL = "https://home.vhorizon.co.uk"
+
+$SourceQuestion = Read-Host -Prompt "Is this server the source or Destination S/D?"
+
+#IF statement imports Storefront Cmdlets and backs up current Storefront Store and configurations to a zip file
+#ELSE statement assumes script is being run as destination mode, the script imports the storefront cmdlets
+# exports the current storefront configuration as a Rollback config (in case of error) then applies the BackUp.zip config 
+
+If ($SourceQuestion -eq 'S') {
+#Execute Storefront cmslets and backup entire storefront configuration
+& 'C:\Program Files\Citrix\Receiver StoreFront\Scripts\ImportModules.ps1'
+Export-STFConfiguration -targetFolder "$BackupFolder" -zipfilename "Backup" -NoEncryption -force
+
+#Copy backup zip file to target StoreFront Server
+Robocopy $BackupFolder "\\$RemoteSFServer\C$\$RemoteDir" Backup.zip /is
+} ELSE {
+& 'C:\Program Files\Citrix\Receiver StoreFront\Scripts\ImportModules.ps1'
+Export-STFConfiguration -targetFolder "$BackupFolder" -zipfilename "Rollback" -NoEncryption -force
+Import-STFConfiguration -ConfigurationZip "$BackupFolder\Backup.zip" -HostBaseUrl "$HostBaseURL"
+Start-DSClusterConfigurationUpdate
+}
+
