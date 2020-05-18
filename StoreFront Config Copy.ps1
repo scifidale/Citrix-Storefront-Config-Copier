9904d219f57fa040584b63d1c239c1e56dadb286
scifidale <49734424+scifidale@users.noreply.github.com>
2020-05-18 11:45:32 +0100

    Update StoreFront Config Copy.ps1

--- StoreFront Config Copy.ps1
+++ StoreFront Config Copy.ps1
@@ -13,6 +13,12 @@ $HostBaseURL = "https://home.vhorizon.co.uk"
 
 $SourceQuestion = Read-Host -Prompt "Is this server the source or Destination S/D?"
 
+IF (Test-Path $BackupFolder -eq True) {} ELSE
+{New-Item -path $BackupFolder -ItemType Directory}
+
+If (Test-Path $RemoteSFServer\C$\$RemoteDir -eq True){} Else
+{New-Item -path $RemoteSFServer\C$\$BackupFolder -ItemType Directory}
+
 #IF statement imports Storefront Cmdlets and backs up current Storefront Store and configurations to a zip file
 #ELSE statement assumes script is being run as destination mode, the script imports the storefront cmdlets
 # exports the current storefront configuration as a Rollback config (in case of error) then applies the BackUp.zip config 
